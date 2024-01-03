bcfcd9215e3377dbc92ac991733bf8e23edb03b1
nic <nicholas.m.williams@colorado.edu>
2024-01-03 12:33:40 -0700

    changing format of install

--- seclook.rb
+++ seclook.rb
@@ -1,8 +1,8 @@
 cask "seclook" do
-    version "1.0.0"
+    version "1.0"
     sha256 "d01e127925f6c201cc80ec1dc7d4849e4e208e2833ebae5ab4f90e3c8ab0c007"
   
-    url "https://github.com/ackatz/seclook/tree/main/Releases/seclook.dmg"
+    url "https://github.com/nicknacnic/seclook/archive/refs/tags/1.0.tar.gz"
     name "seclook"
     desc "Automatic security lookups from your clipboard."
     homepage "https://github.com/ackatz/seclook"
