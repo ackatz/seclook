d52ee557e2f9f69da05f45affb66b554b1e36a30
nic <nicholas.m.williams@colorado.edu>
2024-01-03 12:39:37 -0700

    sha update

--- seclook.rb
+++ seclook.rb
@@ -1,6 +1,6 @@
 cask "seclook" do
     version "1.1"
-    sha256 "2513b62d54613a6e5eb30fef832e59f062e37066c913fd204dd22cec7dc6cb20"
+    sha256 "4db38da0c68a612812c2cbdb0aa8cbbc45f0d7ca86dd79717f2cf009527ba0be"
   
     url "https://github.com/nicknacnic/seclook/archive/refs/tags/1.1.tar.gz"
     name "seclook"
