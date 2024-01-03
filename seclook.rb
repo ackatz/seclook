49b09988cf1b1f8a8961549690a2b09e6c45a8f1
nic <nicholas.m.williams@colorado.edu>
2024-01-03 12:38:58 -0700

    update hierarchy and releases

--- seclook.rb
+++ seclook.rb
@@ -1,8 +1,8 @@
 cask "seclook" do
-    version "1.0"
+    version "1.1"
     sha256 "2513b62d54613a6e5eb30fef832e59f062e37066c913fd204dd22cec7dc6cb20"
   
-    url "https://github.com/nicknacnic/seclook/archive/refs/tags/1.0.tar.gz"
+    url "https://github.com/nicknacnic/seclook/archive/refs/tags/1.1.tar.gz"
     name "seclook"
     desc "Automatic security lookups from your clipboard."
     homepage "https://github.com/ackatz/seclook"
