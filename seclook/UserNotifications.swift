84ef425daba9334077ef9ee91e9c0fd68500caa4
ackatz <andrew@akatz.org>
2024-05-17 13:38:05 -0600

    Add ThreatFox and GreyNoise

--- seclook/UserNotifications.swift
+++ seclook/UserNotifications.swift
@@ -13,6 +13,12 @@ func postNotification(for item: String, type: String, source: String) {
         case "VirusTotal":
             notificationContent.title = "Security Alert from VirusTotal"
             notificationContent.body = "The \(type) \(item) is potentially malicious according to VirusTotal."
+        case "ThreatFox":
+            notificationContent.title = "Security Alert from ThreatFox"
+            notificationContent.body = "The \(type) \(item) is potentially malicious according to ThreatFox."
+        case "GreyNoise":
+            notificationContent.title = "Security Alert from GreyNoise"
+            notificationContent.body = "The \(type) \(item) is potentially malicious according to GreyNoise."
         default:
             notificationContent.title = "Security Alert"
             notificationContent.body = "The \(type) \(item) is potentially malicious."
